f08442691057f025d3834805f5e43b1e5b74d56e
beca_d <67708213+Beca-d@users.noreply.github.com>
2022-10-12 20:54:59 -0400

    connection:

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost:27017/makeadeal123',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  },
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/TradeWay",
+  (err) => {
+    if (err) throw err;
+    console.log("connected to MongoDB");
+  }
 );
 
 mongoose.set("debug", true);
